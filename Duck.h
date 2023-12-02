ce1d7515d55ac65816cf00761228ddc1abb4e51f
Andrea Poklar <a.poklar@yahoo.com>
2023-12-02 13:34:35 -0600

    added duck to game

---
+++ Duck.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// include the proper header file
+#include "FantasyPet.h"
+
+#ifndef DUCK_H
+#define DUCK_H
+
+class Duck : public FantasyPet{
+   public:
+        Duck();
+        void goSwimming();
+        void feedBread();
+        void haveRest();
+};
+
+#endif
